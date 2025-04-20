b33d880c6bbe9900c6f2e42bad05c12c87a25961
YassmeanA <yassmeanahmed23@gmail.com>
2025-04-20 12:52:17 +0200

    Update Logo.js

--- JS/Logo.js
+++ JS/Logo.js
@@ -95,7 +95,6 @@ Containers[index].classList.add("active");
 });
 
 Containers.forEach((Container,index) => {
-Container.style.height="0";
 if(Containers[index].classList.contains("active")){Containers[index].style.pointerEvents="none";};});
 
 LogoContainer.addEventListener("scroll",() => {
@@ -120,7 +119,6 @@ LogoContainer.style.pointerEvents="none";
 show.style.pointerEvents="auto";
 
 Containers.forEach((Container,index) => {
-Container.style.height="auto";
 if(Containers[index].classList.contains("active")){Containers[index].style.pointerEvents="auto";};});
 
 show.style.clipPath=`circle(${Y}px at calc(50vw - 100px + 67px - ${Z}px) calc(var(--i) - ${X}px + 104.034px))`;
@@ -160,7 +158,6 @@ show.style.pointerEvents="none";
 LogoContainer.style.pointerEvents="auto";
 
 Containers.forEach((Container,index) => {
-Container.style.height="0";
 if(Containers[index].classList.contains("active")){Containers[index].style.pointerEvents="none";};});
 
 show.style.transition="clip-path 0.5s, opacity 0.5s 0.5s";

55d20ba47768718c0038398739069d54a539cb91
YassmeanA <yassmeanahmed23@gmail.com>
2025-05-06 19:51:09 +0200

    Update Basic2.js

--- Basic2.js
+++ Basic2.js
@@ -66,7 +66,7 @@ const totalCardWidth = carousel.scrollWidth;
 let slidesInView;
 
 function getSlidesInView() {
-  return Math.round(window.innerWidth / firstSlideWidth);
+  return Math.floor(window.innerWidth / firstSlideWidth);
 }
 
 function setupDots() {
@@ -74,10 +74,11 @@ function setupDots() {
   slidesInView = getSlidesInView();
 
   
-  if (slidesInView >= 0.7 * slides.length) {dotsContainer.style.display = "none";}else{dotsContainer.style.display = "flex";}
+  if (slidesInView >= 0.6 * slides.length) {dotsContainer.style.display = "none";}else{dotsContainer.style.display = "flex";}
 
   if (slidesInView >= slides.length / 2) {slidesInView = slides.length / 2;} 
 
+  if (window.innerWidth >= slides.length * firstSlideWidth + 10) {wrapper.style.width = `${slides.length * (10 + firstSlideWidth) + 10}px`;carousel.style.justifyContent="center";}else{wrapper.style.width = "100vw";carousel.style.justifyContent="start";}
 
   const totalDots = Math.round(slides.length / slidesInView);
   const dots = [];

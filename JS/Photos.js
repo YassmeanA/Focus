99cdc2df45bfaedef367ea260d2a4ecb569e4444
YassmeanA <yassmeanahmed23@gmail.com>
2025-08-19 19:41:10 +0200

    Update Photos.js

--- JS/Photos.js
+++ JS/Photos.js
@@ -3,7 +3,7 @@ let slider10 = document.querySelector(".container10 .slider");
 
 setInterval(() => {
 
-let Slides = document.querySelectorAll(".container10 .slides");
+let Slides = document.querySelectorAll(".container10 .slide");
 slider10.prepend(Slides[Slides.length - 1]);
 
 },1500)

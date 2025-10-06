dfcf4f35a2aebc703806a975c7191d48ca2e34d5
YassmeanA <yassmeanahmed23@gmail.com>
2025-10-06 18:10:40 +0200

    Update Smart.js

--- JS/Smart.js
+++ JS/Smart.js
@@ -183,6 +183,7 @@ Mouse5.style.transform="translate(0,260px)";
 setTimeout(() => {
 
 Mouse5.style.transform="translate(0,10px)";
+Back5.style.transform = "translateY(-250px)";
 detail5s[1].style.transform = "translateY(-250px)";
 
 setTimeout(() => {
@@ -230,6 +231,7 @@ Mouse5.style.transform="translate(0,-100px)";
 setTimeout(() => {
 
 Mouse5.style.transform="translate(0,100px)";
+Back5.style.transform = "translateY(0px)";
 detail5s[1].style.transform = "translateY(0px)";
 
 setTimeout(() => {
@@ -268,7 +270,8 @@ Mouse5.style.transform="translate(0,250px)";
 setTimeout(() => {
 
 Mouse5.style.transform="translate(0,-250px)";
-detail5s[4].style.transform = "translateY(500px)";
+Back5.style.transform = "translateY(-500px)";
+detail5s[4].style.transform = "translateY(-500px)";
 
 setTimeout(() => {
 
@@ -277,6 +280,7 @@ Mouse5.style.transform="translate(0,-250px)";
 setTimeout(() => {
 
 Mouse5.style.transform="translate(0,250px)";
+Back5.style.transform = "translateY(0px)";
 detail5s[4].style.transform = "translateY(0px)";
 
 setTimeout(() => {

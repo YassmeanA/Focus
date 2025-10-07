abec4ab884f67109733e0f40c8d1acb6b6740f6c
YassmeanA <yassmeanahmed23@gmail.com>
2025-10-07 10:31:23 +0200

    Update Basic1.js

--- JS/Basic1.js
+++ JS/Basic1.js
@@ -16,19 +16,18 @@ arrowBtns[1].classList.remove("active");
 currentLeft -= 232; // negative to move content to the left
 carouselA.style.left = currentLeft + "px";
 
+ArrowsDots();
+  
 // when we reach the end, smoothly reset to 0
 if (Math.abs(currentLeft) > 1624) {
-
+currentLeft = 0;
+carouselA.style.left = "0px";
 arrowBtns[1].classList.remove("active");
 arrowBtns[0].classList.add("active");
 setTimeout(() => {
 arrowBtns[0].classList.remove("active");
-currentLeft = 0;
-carouselA.style.left = "0px";
 },300);
 }
-
-ArrowsDots();
 
 },1500);
 
@@ -36,7 +35,7 @@ const ArrowsDots = () => {
 
 const offset = Math.abs(currentLeft);
 arrowBtns[0].style.display = offset <= 100 ? "none" : "block";
-arrowBtns[1].style.display = offset >= 1550 ? "none" : "block";
+//arrowBtns[1].style.display = offset >= 1550 ? "none" : "block";
 
 x++;
 if(x == 8){x = 0;}

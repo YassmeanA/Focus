e60d2fc18eaa06201da5456207cc75bf2cb557a5
YassmeanA <yassmeanahmed23@gmail.com>
2025-10-07 16:48:49 +0200

    Update Slides1.js

--- JS/Slides1.js
+++ JS/Slides1.js
@@ -4,12 +4,12 @@ const Wrapper = document.querySelector(".container4 .carousel");
 let y = 0;
 
 setInterval(() => {
+ 
+y++;
 
 document.querySelector(".container4 .card.active").classList.remove("active");
 Cards[y].classList.add("active");
-  
-y++;
-
+ 
 if(y == 7){Wrapper.style.left = `${-42*y}px`;}else{Wrapper.style.left = `${-50*y}px`;}
 
 if(y == 8){Wrapper.style.left = "-342px";y = -1;}

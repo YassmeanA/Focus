31c9cc8b2e827a509c506d0b97018880ff357378
YassmeanA <yassmeanahmed23@gmail.com>
2025-10-07 13:57:56 +0200

    Update Basic2.js

--- JS/Basic2.js
+++ JS/Basic2.js
@@ -1,20 +1,20 @@
 const carouselB = document.querySelector(".container2 .carousel");
 const DotBs = document.querySelectorAll(".container2 .dot");
 
-let currentLeftB = -20; // Track the current left offset
+let currentLeftB = 0; // Track the current left offset
 let xb = 0;
 
 setInterval(() => {
 
 // move left position by 232 px
-currentLeftB -= 290; // negative to move content to the left
+if(Math.abs(currentLeftB) == 0){currentLeftB -= 290;}else{currentLeftB -= 310;} // negative to move content to the left
 carouselB.style.left = currentLeftB + "px";
 
 dotBs();
 
 // when we reach the end, smoothly reset to 0
-if (Math.abs(currentLeftB) > 1000) {
-currentLeftB = -20;
+if (Math.abs(currentLeftB) > 880) {
+currentLeftB = 0;
 carouselB.style.left = "0px";
 }
  

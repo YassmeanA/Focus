324a93d4c5a8818bc55a62486d847de61af3e5b3
YassmeanA <yassmeanahmed23@gmail.com>
2025-10-07 10:44:00 +0200

    Update Basic1.js

--- JS/Basic1.js
+++ JS/Basic1.js
@@ -2,7 +2,6 @@ const carouselA = document.querySelector(".container1 .carousel");
 const arrowBtns = document.querySelectorAll(".container1 .wrapper .arrow");
 const Dots = document.querySelectorAll(".container1 .dot");
 
-carouselA.style.background="red";
 let currentLeft = 0; // Track the current left offset
 let x = 0;
 
@@ -17,7 +16,7 @@ arrowBtns[1].classList.remove("active");
 currentLeft -= 232; // negative to move content to the left
 carouselA.style.left = currentLeft + "px";
 
-ArrowsDots();
+Dots();
   
 // when we reach the end, smoothly reset to 0
 if (Math.abs(currentLeft) > 1624) {
@@ -30,13 +29,11 @@ arrowBtns[0].classList.remove("active");
 },300);
 }
 
+setTimeout(() => {Arrows();},300);
+  
 },1500);
 
-const ArrowsDots = () => {
-
-const offset = Math.abs(currentLeft);
-arrowBtns[0].style.display = offset <= 100 ? "none" : "block";
-arrowBtns[1].style.display = offset >= 1550 ? "none" : "block";
+const Dots = () => {
 
 x++;
 if(x == 8){x = 0;}
@@ -46,3 +43,10 @@ Dots[x].classList.add("active");
 
 };
 
+const Arrows = () => {
+
+const offset = Math.abs(currentLeft);
+arrowBtns[0].style.display = offset <= 100 ? "none" : "block";
+arrowBtns[1].style.display = offset >= 1550 ? "none" : "block";
+
+};
